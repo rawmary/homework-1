da13035310999bf27ab382cae948a9b349c27b83
rawmary <kurakina1801@gmail.com>
2026-02-03 14:05:08 +0300

    homework-1 start

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import MainLayout from "../shared/layouts/MainLayout"
+import PostList from "../widgets/PostList/PostList"
+
+let App = () => {
+
+  return (
+    <MainLayout>
+      <PostList />
+    </MainLayout>
+  )
+}
+
+export default App
