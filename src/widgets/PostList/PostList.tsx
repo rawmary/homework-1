10b31a63ac36b7efa8cf68412596a562ffe48da5
rawmary <kurakina1801@gmail.com>
2026-02-03 15:57:04 +0300

    Add Post 4 to PostList

--- src/widgets/PostList/PostList.tsx
+++ src/widgets/PostList/PostList.tsx
@@ -4,6 +4,7 @@ const list = [
     { id: 1, title: 'Post 1', content: 'This is the content of post 1.' },
     { id: 2, title: 'Post 2', content: 'This is the content of post 2.' },
     { id: 3, title: 'Post 3', content: 'This is the content of post 3.' },
+    { id: 4, title: 'Post 4', content: 'This is the content of post 4.' },
 ];
 
 const PostList = () => {
